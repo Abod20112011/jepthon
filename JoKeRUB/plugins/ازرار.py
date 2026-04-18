a15916fc1ae358fe2d7e6990d7040fab48310568
Abod Mofed <amdj27840@gmail.com>
2026-04-18 15:00:33 +0300

    Add ازرار.py for bot inline functionality

---
+++ JoKeRUB/plugins/ازرار.py
@@ -0,0 +1,60 @@
+from JoKeRUB import l313l, bot
+import time
+from telethon.tl import types
+from JoKeRUB import BOTLOG_CHATID
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+import asyncio
+from ..Config import Config
+import requests
+from telethon import Button, events
+from telethon.tl.functions.messages import ExportChatInviteRequest
+from ..core.managers import edit_delete, edit_or_reply
+#ياعلي
+#اخ اخ اخ اخ اخ اخ اخممممممط ياطويل العمر اخمطط 😂
+#Reda
+REH = "**᯽︙ لأستخدام بوت اختراق الحساب عن طريق كود التيرمكس أضغط على الزر**"
+JOKER_PIC = "https://files.catbox.moe/unov55.jpg"
+Bot_Username = Config.TG_BOT_USERNAME
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+    
+    @tgbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        joker = Bot_Username.replace("@", "")
+        query = event.text
+        await bot.get_me()
+        if query.startswith("ازرار") and event.query.user_id == bot.uid:
+            buttons = Button.url("• الـمطـور •", f"https://t.me/BD_0I}")
+            if JOKER_PIC and JOKER_PIC.endswith((".jpg", ".png", "gif", "mp4")):
+                result = builder.photo(
+                    JOKER_PIC, text=REH, buttons=buttons, link_preview=False
+                )
+            elif JOKER_PIC:
+                result = builder.document(
+                    JOKER_PIC,
+                    title="Aljoker 🤡",
+                    text=REH,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            else:
+                result = builder.article(
+                    title="Aljoker 🤡",
+                    text=REH,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+        await event.answer([result] if result else None)
+
+@bot.on(admin_cmd(outgoing=True, pattern="هاك"))
+async def repo(event):
+    if event.fwd_from:
+        return
+    lMl10l = Config.TG_BOT_USERNAME
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    await bot.send_message(lMl10l, "/hack")
+    response = await bot.inline_query(lMl10l, "هاك")
+    await response[0].click(event.chat_id)
+    await event.delete()
